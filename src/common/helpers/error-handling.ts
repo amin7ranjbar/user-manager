7c730ee5c9814b1a28a5a633ed33c792705d3c64
amin.ranjbar <amin7ranjbar@gmail.com>
2022-04-28 10:46:32 +0430

    error handing changes

--- src/common/helpers/error-handling.ts
+++ src/common/helpers/error-handling.ts
@@ -3,17 +3,14 @@ import logger from "./logger";
 
 export function loadErrorHandlers(app: Application) {
   app.use((err: any, req: Request, res: Response, next: any) => {
-    if (err.name === "ValidationError") {
-      err.status = 400;
-    }
     logger.error(err);
-
-    res.status(err.status || 500);
-    res.json({
-      errors: {
-        message: err.message,
-        error: !(process.env.APP_ENV == "production") ? err : {},
-      },
-    });
+    const status =
+      err.name === "ValidationError"
+        ? 400
+        : err.name === "AuthorizationError"
+        ? 401
+        : err.status || 500;
+    const message = status == 500 ? "unexpected error" : err.message;
+    res.status(status).json({ message });
   });
 }
